4e368e561eb0b2f1482e4a80c814b13c0925c364
tytyereal <58371547+tytyereal@users.noreply.github.com>
2022-05-26 15:55:52 -0400

    Add project files.

---
+++ HardMode/HardMode.cs
@@ -0,0 +1,89 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace HardMode
+{
+    public class ModTest : MelonMod
+    {
+        //Redefines Backpack tile gain to be flatter and more difficult early game
+        [HarmonyPatch(typeof(Player), nameof(Player.AddExperience))]
+        class TileLevelUpCountPatch
+        {
+            static bool Prefix(ref Player __instance)
+            {
+                MelonLogger.Msg("Player.Start");
+                __instance.gridsToGain = new int[]
+                {
+                    0, 0, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3
+                };
+
+                return true;
+            }
+        }
+
+        //Limits the the amount of items you can take to 2
+        [HarmonyPatch(typeof(GameManager), nameof(GameManager.ChangeItemsAllowedToTake))]
+        class ItemsAllowedToTakePatch
+        {
+            static bool Prefix(ref GameManager __instance)
+            {
+                if (__instance.numOfItemsAllowedToTake == 3)
+                {
+                    __instance.numOfItemsAllowedToTake = 2;
+                }
+                return true;
+
+            }
+        }
+
+        //Changes default AP per turn
+        [HarmonyPatch(typeof(Player), nameof(Player.NextTurn))]
+        class ApChangePatch
+        {
+            static bool Prefix(ref Player __instance)
+            {
+                __instance.SetAP(2);
+                return false;
+            }
+        }
+
+        //Checks if you are starting a new game
+        public static bool startNewOrMatt = false;
+        [HarmonyPatch(typeof(MenuManager), nameof(MenuManager.LoadGame))]
+        class StartNewGameCheckPatch
+        {
+            static void Postfix()
+            {
+                startNewOrMatt = true;
+            }
+        }
+        //Checks if you are starting a matthew game
+        [HarmonyPatch(typeof(MenuManager), nameof(MenuManager.LoadMatt))]
+        class StartMattGameCheckPatch
+        {
+            static void Postfix()
+            {
+                startNewOrMatt = true;
+            }
+        }
+        //Sets Max health to 20 if starting a new run
+        [HarmonyPatch(typeof(Status), "Start")]
+        class MaxHpPatch
+        {
+            static void Postfix(ref Status __instance)
+            {
+                if (!startNewOrMatt) return;
+                UnityEngine.Object.FindObjectOfType<Player>().stats.maxHealth = 20;
+                startNewOrMatt = false;
+                __instance.ClampHealth();
+            }
+        }
+    }
+}
---
+++ HardMode/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿using HardMode;
+using MelonLoader;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HardMode")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HardMode")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1d8319c4-9ad5-493b-965c-71c300e6cd36")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonInfo(typeof(ModTest), "HardMode", "1.0.0", "tytyereal")]
+[assembly: MelonGame("", "")]
