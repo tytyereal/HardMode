fb7f14f93f4463e2494a8def18264b20227b88ca
tytyereal <58371547+tytyereal@users.noreply.github.com>
2022-05-26 16:29:51 -0400

    Update HardMode.cs

--- HardMode/HardMode.cs
+++ HardMode/HardMode.cs
@@ -1,12 +1,5 @@
 ﻿using HarmonyLib;
 using MelonLoader;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TMPro;
-using UnityEngine;
 
 namespace HardMode
 {
